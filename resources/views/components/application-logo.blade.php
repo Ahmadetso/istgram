118d805cf5ffab1821736d57fe05f32f7c7bac86
admin <ahmdgamrs84@gmail.com>
2024-01-11 01:49:23 +0300

    auth and nullable

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,10 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="200" height="auto" viewBox="0 0 250 60" {{$attributes}}>
+    <!-- Link the desired font -->
+    <style>
+      @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+    </style>
+    
+    <!-- Use the text with italic transformation -->
+    <text x="40" y="40" font-family="Pacifico, cursive" font-size="36" fill="#405DE6">ahmadgram</text>
+  </svg>
+  
